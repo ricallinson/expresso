34ece166e80b8c9610d8d1b5bfc2fdf832ad733a
Ric Allinson <allinson@yahoo-inc.com>
2016-04-11 22:56:50 -0700

    Updatd to use new http repo

--- Main.java
+++ Main.java
@@ -1,13 +1,12 @@
 package github.com.ricallinson.expresso;
 
-import github.com.ricallinson.http.Server;
+import github.com.jminusminus.http.Server;
 
 public class Main {
 
     public static void main(String[] args) {
-        Server s = Server.createServer((req, res) -> {
+        Server.createServer((req, res) -> {
             res.end("<h1>Hello world</h1>");
-        });
-        s.listen(8080);
+        }).listen(8080);
     }
 }
